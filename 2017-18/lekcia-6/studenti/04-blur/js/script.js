de99d58ce4de78dd32906f32afc9a4166fd6f743
Dalibor Schön <Dalibor.Schon@performgroup.com>
2017-11-27 11:01:12 +0100

    lekcia-6 podulohy

---
+++ 2017-18/lekcia-6/studenti/04-blur/js/script.js
@@ -0,0 +1,18 @@
+const popup = $('#share-article');
+
+$('article').on('click', (evt) => {
+  const source = $(evt.currentTarget);
+  const bgImage = source.css('background-image');
+  const title = source.find('h2').text();
+  const text = source.find('p').text();
+
+  popup.find('.popup__body').css({'background-image': bgImage});
+  popup.find('.popup__title h2').text(title);
+  popup.find('.popup__title p').text(text);
+
+  popup.show();
+});
+
+$('.popup__close').on('click', () => {
+  popup.hide();
+});
