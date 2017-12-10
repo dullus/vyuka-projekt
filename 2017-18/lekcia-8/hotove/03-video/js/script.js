a1fe501345364c8c7c0c11fb6a64c33b8275c125
Dalibor Schön <dullus@gmail.com>
2017-12-10 22:22:56 +0100

    lekcia-8

---
+++ 2017-18/lekcia-8/hotove/03-video/js/script.js
@@ -0,0 +1,50 @@
+const popup = $('#share-article');
+const main = $('main');
+const videoPopup = $('#video-popup');
+
+$('article').on('click', (evt) => {
+  main.addClass('blur');
+  const source = $(evt.currentTarget);
+  const bgImage = source.css('background-image');
+  const title = source.find('h2').text();
+  const text = source.find('p').text();
+
+  popup.find('.popup__body').css({'background-image': bgImage});
+  popup.find('.popup__title h2').text(title);
+  popup.find('.popup__title p').text(text);
+  popup.show();
+});
+
+$('#popup-close').on('click', () => {
+  popup.addClass('popup--fall');
+  main.removeClass('blur');
+  setTimeout(() => {
+    popup.hide();
+    popup.removeClass('popup--fall');
+  }, 410);
+});
+
+$('#popup-close').tooltip({
+  delay: {
+    show: 500,
+    hide: 100
+  }
+});
+
+$('#home').on('click', () => {
+  $('html, body').animate({ scrollTop: 0 }, 'slow');
+});
+
+$('#video').on('click', () => {
+  popup.hide();
+  main.addClass('blur');
+  videoPopup.find('.popup__body').html(
+'<iframe width="100%" height="230" src="https://www.youtube.com/embed/iZT_Dr59KJ8?rel=0"></iframe>'
+  );
+  videoPopup.show();
+});
+
+$('#video-popup-close').on('click', () => {
+  main.removeClass('blur');
+  videoPopup.hide();
+});
