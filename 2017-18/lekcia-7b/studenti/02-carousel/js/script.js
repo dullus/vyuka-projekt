b491d8d6547330b306a2979541ce2373f7e12eb6
Dalibor Schon <dullus@gmail.com>
2018-06-15 09:33:25 +0200

    7b

---
+++ 2017-18/lekcia-7b/studenti/02-carousel/js/script.js
@@ -0,0 +1,25 @@
+const popup = $('#share-article');
+const main = $('main');
+
+$('article').on('click', (evt) => {
+  main.addClass('blur');
+  const source = $(evt.currentTarget);
+  const bgImage = source.css('background-image');
+  const title = source.find('h2').text();
+  const text = source.find('p').text();
+
+  popup.find('.popup__body').css({'background-image': bgImage});
+  popup.find('.popup__title h2').text(title);
+  popup.find('.popup__title p').text(text);
+  popup.show();
+});
+
+$('.popup__close').on('click', () => {
+  popup.addClass('popup--fall');
+  main.removeClass('blur');
+  setTimeout(() => {
+    popup.hide();
+    popup.removeClass('popup--fall');
+  }, 410);
+});
+
